fc3978505a94fe3b48840ae6e623fbf4d57a297d
Bryan verneck <bryanverneck@gmail.com>
2021-02-17 21:42:50 -0300

    work in progress

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import User from '../src/components/user'
 export default function App() {
   return (
     <div>
+      <span style={{color: "yellow", padding: "20px"}}>Work in progress</span>
       <User></User>
     </div>
   )
