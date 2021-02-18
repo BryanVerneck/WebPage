4338f26054c6fafa9d696045ad95356e8b4b9c82
Bryan verneck <bryanverneck@gmail.com>
2021-02-17 21:21:12 -0300

    project init and user added

---
+++ src/components/user/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from '../../../styles/Home.module.css'
+
+export default function User(){
+    return(      
+        <div className={styles.user}>
+            <span className={styles.avatar}>
+                <img src="https://avatars.githubusercontent.com/u/40044852?s=400&u=99e63fe119e4c75b29e6b13d7f043274dd6b06dd&v=4" className={styles.img}></img>
+            </span>
+            <div>
+                <h2 className={styles.name}>
+                    Bryan Verneck
+                </h2>
+                <p style={{color: "white"}}>Full Stack developer</p>
+            </div>
+        </div>   
+    )
+}
